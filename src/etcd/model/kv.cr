fff88447e20f49604844ced57459e677fa9f89fc
Anh (Duke) Nguyen <58082199+dukeraphaelng@users.noreply.github.com>
2021-03-08 15:17:08 +1100

    refactor(kv.cr): use correct WithHeader struct, change Kv struct getter order

--- src/etcd/model/kv.cr
+++ src/etcd/model/kv.cr
@@ -2,18 +2,19 @@ require "./base"
 
 module Etcd::Model
   struct Kv < Base
-    @[JSON::Field(converter: Etcd::Model::Base64Converter)]
-    getter key : String
-    @[JSON::Field(converter: Etcd::Model::Base64Converter)]
-    getter value : String?
+    # Why are these values nillable
     @[JSON::Field(converter: Etcd::Model::StringTypeConverter(UInt64))]
     getter create_revision : UInt64?
-    @[JSON::Field(converter: Etcd::Model::StringTypeConverter(UInt64))]
-    getter mod_revision : UInt64?
-    @[JSON::Field(converter: Etcd::Model::StringTypeConverter(Int64))]
-    getter version : Int64?
+    @[JSON::Field(converter: Etcd::Model::Base64Converter)]
+    getter key : String
     @[JSON::Field(converter: Etcd::Model::StringTypeConverter(Int64))]
     getter lease : Int64?
+    @[JSON::Field(converter: Etcd::Model::StringTypeConverter(UInt64))]
+    getter mod_revision : UInt64?
+    @[JSON::Field(converter: Etcd::Model::Base64Converter)]
+    getter value : String?
+    @[JSON::Field(converter: Etcd::Model::StringTypeConverter(Int64))]
+    getter version : Int64?
   end
 
   struct RangeResponse < Base
@@ -23,17 +24,18 @@ module Etcd::Model
     getter kvs : Array(Etcd::Model::Kv) = [] of Etcd::Model::Kv
   end
 
-  struct PutResponse < Withheader
+  struct PutResponse < WithHeader
+    # Why is this nillable
     getter prev_kv : Kv?
   end
 
-  struct DeleteResponse < Withheader
+  struct DeleteResponse < WithHeader
     @[JSON::Field(converter: Etcd::Model::StringTypeConverter(Int32))]
     getter deleted : Int32 = 0
     getter prev_kvs : Array(Etcd::Model::Kv) = [] of Etcd::Model::Kv
   end
 
-  struct TxnResponse < Withheader
+  struct TxnResponse < WithHeader
     getter succeeded : Bool = false
 
     alias Response = NamedTuple(
